83a382ba72f1e8a5ed8534e01f3a1b143f5e9de5
Marina <maryna_kuzmiankova@mail.ru>
2022-10-28 15:56:01 +0300

    Updates

---
+++ static/js/926.93689707.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklesson=self.webpackChunklesson||[]).push([[926],{4926:function(n,e,s){s.r(e);var u=s(184);e.default=function(){return(0,u.jsx)("h4",{children:"\u041a\u043e\u043c\u043f\u043e\u043d\u0435\u043d\u0442 \u0431\u044b\u043b \u0437\u0430\u0433\u0440\u0443\u0436\u0435\u043d \u0441 \u043f\u043e\u043c\u043e\u0449\u044c\u044e Lazy Loading"})}}}]);
+//# sourceMappingURL=926.93689707.chunk.js.map
